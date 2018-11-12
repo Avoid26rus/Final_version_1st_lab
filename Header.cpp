7110ec14b89b077c13da4c5f315d9757e069fb3b
Avoid26rus <43587748+Avoid26rus@users.noreply.github.com>
2018-11-13 02:45:57 +0300

    Create Header.cpp

---
+++ Header.cpp
@@ -0,0 +1,19 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define _GLIBCXX_USE_CXX11_ABI 0
+#include <iostream>
+#include <locale>
+#include <fstream>
+#include <cstdio>
+#include <conio.h>
+#include <string>
+#include <windows.h>
+#include <cmath>
+
+#pragma warning (disable : 4996)
+
+const int NotUsed = system("color 1E");
+
+#define N 5
+
+using namespace std;
